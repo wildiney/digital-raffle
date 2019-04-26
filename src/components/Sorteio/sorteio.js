a14887b1c3960ac1022f7cc5488e969f72a80b4c
Fernando Di Masi <wildiney@gmail.com>
2019-04-26 16:48:15 -0300

    Serving data from json file

--- src/components/Sorteio/sorteio.js
+++ src/components/Sorteio/sorteio.js
@@ -1,30 +1,48 @@
 import React, { Component } from 'react';
 import Display from './Display/display';
 import Aux from '../../hoc/Auxiliary';
-import classes from './Sorteio.module.css'
+import classes from './Sorteio.module.css';
 
 class Sorteio extends Component {
-  constructor(props){
-    super(props)
-    
+  constructor(props) {
+    super(props);
+
     this.state = {
       amountChars: 30,
       participantes: [
-        { nome: 'Wildiney Fernando Pimentel Di Masi', telefone: '11 0000 0000' },
+        /*{
+          nome: 'Wildiney Fernando Pimentel Di Masi',
+          telefone: '11 0000 0000'
+        },
         { nome: 'Nome Sobrenome de 02 mais eu', telefone: '11 1111 1111' },
         { nome: 'Nome Sobrenome de 03 mais eu', telefone: '11 2222 2222' },
         { nome: 'Nome Sobrenome de 04 mais eu', telefone: '11 3333 3333' },
         { nome: 'Nome Sobrenome de 05 mais eu', telefone: '11 4444 4444' },
         { nome: 'Nome Sobrenome de 06 mais eu', telefone: '11 5555 5555' },
-        { nome: 'Nome Sobrenome de 07 mais eu', telefone: '11 6666 6666' }
+        { nome: 'Nome Sobrenome de 07 mais eu', telefone: '11 6666 6666' }*/
       ],
       selectedPerson: { name: 'Boa sorte!', extraField: '00 0000 0000' },
       timeToSort: 5000,
       sorted: false
     };
 
+    this.jsonData = 'http://localhost:4200/participantes';
+    console.log(this.state.participantes)
   }
- 
+
+  componentDidMount() {
+    this.getParticipantes();
+    console.log(this.state.participantes);
+  }
+
+  getParticipantes = () => {
+    console.log('Getting data');
+    fetch(this.jsonData)
+      .then(response => response.json())
+      .then(data => this.setState({ participantes: data }))
+      .then(console.log(this.state.participantes));
+      
+  };
 
   sortear = () => {
     const participantes = this.state.participantes;
@@ -36,15 +54,17 @@ class Sorteio extends Component {
 
   sortNameHandler = () => {
     let int = setInterval(() => {
-      document.getElementById("sorteio").classList.remove("blink");
+      document.getElementById('sorteio').classList.remove('blink');
       this.sortear();
     }, 100);
 
     setTimeout(() => {
       clearInterval(int);
-      document.getElementById("sorteio").classList.add("blink")
-      console.table({ "Name": this.state.selectedPerson.name, "Field": this.state.selectedPerson.extraField })
-
+      document.getElementById('sorteio').classList.add('blink');
+      console.table({
+        Name: this.state.selectedPerson.name,
+        Field: this.state.selectedPerson.extraField
+      });
     }, 2000);
   };
 
@@ -62,16 +82,29 @@ class Sorteio extends Component {
 
     return (
       <Aux>
-        <div id="sorteio" className={[classes.wrapperItem, classes.sorteio].join(' ')}>
+        <div
+          id="sorteio"
+          className={[classes.wrapperItem, classes.sorteio].join(' ')}
+        >
           {display}
         </div>
-        <div className={[classes.wrapperItem, classes.btn_sorteio, classes.btn_sortear].join(' ')}>
-          <button className={[classes.btn].join(' ')} onClick={this.sortNameHandler} >SORTEAR</button>
+        <div
+          className={[
+            classes.wrapperItem,
+            classes.btn_sorteio,
+            classes.btn_sortear
+          ].join(' ')}
+        >
+          <button
+            className={[classes.btn].join(' ')}
+            onClick={this.sortNameHandler}
+          >
+            SORTEAR
+          </button>
         </div>
       </Aux>
     );
   }
-
 }
 
 export default Sorteio;
