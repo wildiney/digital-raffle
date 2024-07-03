a2109fb6410fd37dcfe0da141c7aa305bba94b04
Wildiney Di Masi <wildiney@gmail.com>
2024-07-03 11:10:31 -0300

    update to typescript

--- src/components/Footer/footer.js
+++
@@ -1,8 +0,0 @@
-import classes from './footer.module.css'
-const Footer = () => {
-  return (
-    <footer className="footer"><p className={classes.copyright}>&copy; 2022 Wildiney Di Masi</p></footer>
-  )
-}
-
-export default Footer;
